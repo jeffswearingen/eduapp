19da9bc285cb915c940b3239c470f88075d3eda0
Jeff Swearingen <jeffdevelopmentacct@gmail.com>
2017-03-19 19:06:51 -0400

    add menu and login buttons to header

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,15 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('App', ['ionic'])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    .state('login', {
+    url: '/login',
+    templateUrl: 'views/login.html'
+  })
+})
+
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
